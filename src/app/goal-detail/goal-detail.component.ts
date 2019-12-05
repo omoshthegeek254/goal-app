c1d70dd5d7d1ddcc92cae87ec79044b7682839ff
omoshthegeek <omoshthegeek@gmail.com>
2019-12-05 10:52:46 +0300

    added the goal delete function next is bootstrap adding

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -14,6 +14,10 @@ export class GoalDetailComponent implements OnInit {
   goalComplete(complete:boolean){
     this.isComplete.emit(complete);
   }
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   
   constructor() { }
 
